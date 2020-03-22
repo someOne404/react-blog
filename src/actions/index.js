867096579192aa730b43c75b74b6838e7486e756
someOne404 <jiayilu96@gmail.com>
2020-03-22 18:13:14 -0400

    Memoize API call part 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => { // return a function to be handled by the middleware thunk
@@ -7,3 +8,13 @@ export const fetchPosts = () => { // return a function to be handled by the midd
         dispatch({ type: 'FETCH_POSTS', payload: response.data });
     }
 };
+
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async(id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+});
