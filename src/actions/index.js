34786830713102788f60820cd0b044185259392c
someOne404 <jiayilu96@gmail.com>
2020-03-22 18:41:27 -0400

    Memoize API calls part 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,16 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    // get unique user ids 
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+
+    userIds.forEach(id => dispatch(fetchUser(id))); // no need to put await in front of dispatch, as we don't have any logic after fetching user
+
+};
+
 export const fetchPosts = () => { // return a function to be handled by the middleware thunk
     return async function(dispatch, getState) {
         const response = await jsonPlaceholder.get('/posts');
@@ -9,12 +19,19 @@ export const fetchPosts = () => { // return a function to be handled by the midd
     }
 };
 
-export const fetchUser = (id) => dispatch => {
-    _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async(id, dispatch) => {
+export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
+
+
+// export const fetchUser = (id) => dispatch => {
+//     _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
